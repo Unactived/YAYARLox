9bc56c4ca354c6bd3c4f5445e69a0931957e624e
FrenchMasterSword <frenchmastersword@gmail.com>
2020-05-20 11:05:20 +0200

    Added support for subtypes; Added Block and Assign

--- src/ast.rs
+++ src/ast.rs
@@ -4,14 +4,14 @@ use crate::lexer::Token;
 
 macro_rules! define_ast {
     ( $Category:ident := 
-        $( $expr:ident : $( $type:ident $name:ident ),* );*
+        $( $expr:ident : $( $type:ident$(<$($thing:ident),*>)? $name:ident ),* );*
     ) => {
 
         #[derive(Debug)]
         pub enum $Category {
         $(
             $expr(
-                $(Box<$type>,)*
+                $(Box<$type$(<$($thing),*>)?>,)*
             ),
         )*
         }
@@ -20,6 +20,7 @@ macro_rules! define_ast {
 
 define_ast!(
     Expr :=
+        Assign   : Token name, Expr value ;
         Binary   : Expr left, Token operator, Expr right ;
         Grouping : Expr expr ;
         Literal  : Token value ;
@@ -29,6 +30,7 @@ define_ast!(
 
 define_ast!(
     Stmt :=
+        Block      : Vec<Stmt> statements ;
         Expression : Expr expression ;
         Print      : Expr expression ;
         Var        : Token name, Expr initializer
